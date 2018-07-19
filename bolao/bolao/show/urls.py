604ffca1c5d165f36657b500cb65ee97ffadda9b
larellano1 <lfv.arellano@gmail.com>
2018-07-19 08:38:46 -0300

    Pronto mecanismo de login e logout

--- bolao/bolao/show/urls.py
+++ bolao/bolao/show/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path(r'login', auth_views.login, {'template_name': 'show/login.html'}, name='login'),
+    path(r'logout', auth_views.logout, {'template_name': 'show/index.html'}, name='logout')
 ]
