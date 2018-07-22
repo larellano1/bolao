ca96f5b16f241e3ae54641573efb39a401c40cfd
larellano1 <lfv.arellano@gmail.com>
2018-07-21 21:09:50 -0300

    Altera settigs para usar Mysql

--- bolao/bolao/bolao/settings.py
+++ bolao/bolao/bolao/settings.py
@@ -74,13 +74,22 @@ WSGI_APPLICATION = 'bolao.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'larellano1$bolaodb',
+        'USER': 'larellano1',
+        'PASSWORD': 'Vidal20151223',
+        'HOST': 'larellano1.mysql.pythonanywhere-services.com',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
