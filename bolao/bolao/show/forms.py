78f773186f1138ec15de8e7c37240a67260fb956
larellano12 <34381593+larellano12@users.noreply.github.com>
2018-07-27 14:33:20 -0300

    Alteracao de forms.py para inclusao de formmodel.

--- bolao/bolao/show/forms.py
+++ bolao/bolao/show/forms.py
@@ -1,6 +1,12 @@
 from django import forms
+from .models import Bolao
 
 class ConvidarAmigo(forms.Form):
     nome_amigo = forms.CharField(label="Nome do amigo:", max_length = 200)
     email_amigo = forms.EmailField(label="E-mail do Amigo:")
     mensagem = forms.CharField(label="Escreva uma mensagem:", widget=forms.Textarea)
+
+class BolaoForm(forms.ModelForm):
+    class Meta:
+        model = Bolao
+        fields = ["nome", "campeonato", "participantes", "regulamento", "premiacao", "imagem", "valor_entrada"]
